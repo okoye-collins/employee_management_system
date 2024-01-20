378192be351868dfd24bfe72872279c0bdb5653f
okoye_collins <collins.kcj@gmail.com>
2024-01-21 00:12:07 +0100

    done with the commission and salary payment

--- payroll/urls.py
+++ payroll/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import SalaryPaymentAPIView, SalaryPaymentDetailAPIView
+from .views import SalaryPaymentAPIView, SalaryPaymentDetailAPIView, CommissionPaymentDetailAPIView, CommissionPaymentAPIView
 # , PayrollDetailAPIView
 
 urlpatterns = [
     path('salary_payment/', SalaryPaymentAPIView.as_view(), name="Salary-Payment"),
-    path('salary_payment_/<int:pk>/', SalaryPaymentDetailAPIView.as_view(), name="salary-payment-detail"),
+    path('salary_payment_detail/<int:pk>/', SalaryPaymentDetailAPIView.as_view(), name="salary-payment-detail"),
+    path('commission_payment/', CommissionPaymentAPIView.as_view(), name="commission_payment"),
+    path('commission_payment_detail/<int:pk>/', CommissionPaymentDetailAPIView.as_view(), name="commission_payment_detail"),
+    
 ]
--- payroll/views.py
+++ payroll/views.py
@@ -17,8 +17,8 @@ class SalaryPaymentAPIView(GenericAPIView):
     serializer_class = SalaryPaymentSerializer
 
     def post(self, request):
-        serializer = self.serializer_class(data=self.request.data)
-        serializer.is_valid(raise_exception=True)
+        serializer = self.serializer_class(data=request.data)
+        serializer.validate(raise_exception=True)
         serializer.save()
 
         return Response({'message': 'Salary Payment has been created Successfully'}, status=status.HTTP_201_CREATED)
@@ -43,7 +43,7 @@ class SalaryPaymentDetailAPIView(GenericAPIView):
             return Response({'message': 'Salary Payment not found'}, status=status.HTTP_404_NOT_FOUND)
 
         serializer = self.serializer_class(salary_payment, data=request.data)
-        serializer.is_valid(raise_exception=True)
+        serializer.validate(raise_exception=True)
         serializer.save()
         return Response({'message': 'Salary payment has been Updated successfully'}, status=status.HTTP_200_OK)
 
@@ -64,6 +64,40 @@ class CommissionPaymentAPIView(GenericAPIView):
 
     def post(self, request):
         serializer = self.serializer_class(data=request.data)
-        serializer.is_valid(raise_exception=True)
+        serializer.validate(raise_exception=True)
         serializer.save()
+
+        return Response({'message': 'Commission Payment has been created Successfully'}, status=status.HTTP_201_CREATED)
+
+    def get(self, request):
+
+        commission_payment = CommissionPayment.objects.all()
+        serializer = self.serializer_class(commission_payment, many=True)
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+
+class CommissionPaymentDetailAPIView(GenericAPIView):
+
+    permission_classes = (IsAdminUser,)
+    serializer_class = CommissionPaymentSerializer
+
+    def put(self, request, pk):
+        try:
+            commission_payment = CommissionPayment.objects.get(id=pk)
+        except Exception as e:
+            return Response({'Message': 'Commission Payement not found'}, status=status.HTTP_404_NOT_FOUND)
         
+        serializer = self.serializer_class(commission_payment, data=request.data)
+        serializer.validate(raise_exception=True)
+        serializer.save()
+        return Response({'message': 'Commission payment has been Updated successfully'}, status=status.HTTP_200_OK)
+    
+    def delete(self, request, pk):
+        try:
+            commission_payment = CommissionPayment.objects.get(id=pk)
+        except Exception as e:
+            return Response({'message': 'Commission Payment not found'}, status=status.HTTP_404_NOT_FOUND)
+
+        commission_payment.delete()
+        return Response({'message': 'Commission Payment has been deleted successfully'}, status=status.HTTP_200_OK)
