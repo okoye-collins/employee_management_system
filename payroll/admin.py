a3fb3b49525e03829e0e8e34e3dcd3786bee80b2
okoye_collins <collins.kcj@gmail.com>
2024-01-22 21:02:50 +0100

    added order models to the admin.py

--- payroll/admin.py
+++ payroll/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Payroll
+from .models import Payroll, CommissionPayment, SalaryPayment
 
 # Register your models here.
 
 admin.site.register(Payroll)
+admin.site.register(CommissionPayment)
+admin.site.register(SalaryPayment)
